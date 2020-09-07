b65d0e5c3abf52a096cc55cb2ad03eb289940042
pamtsaki <karakasstef@hotmail.com>
2020-09-07 15:01:11 +0300

    A message describing what you have done to make this snapshot different

--- script.js
+++ script.js
@@ -6,73 +6,66 @@ console.log("This is a rock-paper-scissors game.");
 // paper > rock
 // equals = draw!
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
-    
-    switch(playerSelection) {
-        case "ROCK":
-            if (computerSelection == "ROCK") {
-                return "tie";
-            }
-            else if (computerSelection == "PAPER") {
-                return "lose";
-            }
-            else {
-                return "win";
-            }
-        case "PAPER":
-            if (computerSelection == "ROCK") {
-                return "win";
-            }
-            else if (computerSelection == "PAPER") {
-                return "tie";
-            }
-            else {
-                return "lose";
-            }
-        case "Scissors":
-            if (computerSelection == "ROCK") {
-                return "lose";
-            }
-            else if (computerSelection == "PAPER") {
-                return "win";
-            }
-            else {
-                return "tie";
-            }
-    }
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = computerSelection.toUpperCase();
+
+  switch (playerSelection) {
+    case "ROCK":
+      if (computerSelection == "ROCK") {
+        return "tie";
+      } else if (computerSelection == "PAPER") {
+        return "lose";
+      } else {
+        return "win";
+      }
+    case "PAPER":
+      if (computerSelection == "ROCK") {
+        return "win";
+      } else if (computerSelection == "PAPER") {
+        return "tie";
+      } else {
+        return "lose";
+      }
+    case "Scissors":
+      if (computerSelection == "ROCK") {
+        return "lose";
+      } else if (computerSelection == "PAPER") {
+        return "win";
+      } else {
+        return "tie";
+      }
+  }
 }
 
 //list for computer's play
 const choices = ["rock", "paper", "scissors"];
 
 //randomly pick a choice to play
-function computerPlay(listOfChoices){
-    let choice = Math.floor(Math.random() * listOfChoices.length);
-    return listOfChoices[choice];
+function computerPlay(listOfChoices) {
+  let choice = Math.floor(Math.random() * listOfChoices.length);
+  return listOfChoices[choice];
 }
 
 let playerSelection;
 let scorePlayer = 0;
 let scoreComputer = 0;
 
-function game(){
-    playerSelection = prompt("Choose rock,paper,scissors: ");
-    const result = playRound(playerSelection, computerPlay(choices));
-    if (result == "tie"){
-        console.log('Tie')
-    }else{
-        console.log(`You ${result}!`);
-    }
+function game() {
+  playerSelection = prompt("Choose rock,paper,scissors: ");
+  const result = playRound(playerSelection, computerPlay(choices));
+  if (result == "tie") {
+    console.log("Tie");
+  } else {
+    console.log(`You ${result}!`);
+  }
 
-    if(result == "win"){
-        scorePlayer++;
-    }else if(result == "lose"){
-        scoreComputer++;
-    }
-    console.log(scorePlayer);
+  if (result == "win") {
+    scorePlayer++;
+  } else if (result == "lose") {
+    scoreComputer++;
+  }
+  console.log(scorePlayer);
 }
-
 
 /* WORKING CODE
 const playerSelection = "paper";
@@ -81,8 +74,8 @@ console.log(playRound(playerSelection, computerSelection));
 */
 
 let i;
-for (i=0; i<5; i++){
-    game();
+for (i = 0; i < 5; i++) {
+  game();
 }
 
 console.log(`Final score, YOU ${scorePlayer} : CPU ${scoreComputer}`);
